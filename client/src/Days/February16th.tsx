69dd1ad4bd38c08a0955cfceae5b73ccc388f377
xennia316 <xennia316@gmail.com>
2024-02-16 20:59:26 +0100

    increment failed days

--- client/src/Days/February16th.tsx
+++ client/src/Days/February16th.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function February15() {
+function February16() {
 	return (
 		<div className="App">
 			<header className="App-header">
 				<b>
 					<p>
 						I think I have failed to<span className="App-link"> mouth</span>,
-						<br /> accomplish something <span className="App-link">today</span>.
+						<br /> accomplish something{" "}
+						<span className="App-link">today</span>.
 					</p>
 				</b>
 			</header>
@@ -17,4 +18,4 @@ function February15() {
 	);
 }
 
-export default February15;
+export default February16;
